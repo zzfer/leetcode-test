8ec61de0d759f2cf575384a69923558fd2490dae
zhaozhifei <1160306458@qq.com>
2022-04-26 17:36:52 +0800

    350、数组交集

--- src/com/zzf/ArrayIntersection.java
+++ src/com/zzf/ArrayIntersection.java
@@ -89,15 +89,15 @@ public class ArrayIntersection {
         return Arrays.copyOfRange(result, 0, count);
     }
 
-    public static int[] intersectTwe(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0 ,j = 0;
-
-        while (i < nums1.length && j < nums2.length){
-            if(nums1[i] == nums2[j]){
-                nums1[i]
-            }
-        }
-    }
+//    public static int[] intersectTwe(int[] nums1, int[] nums2) {
+//        Arrays.sort(nums1);
+//        Arrays.sort(nums2);
+//        int i = 0 ,j = 0;
+//
+//        while (i < nums1.length && j < nums2.length){
+//            if(nums1[i] == nums2[j]){
+//                nums1[i]
+//            }
+//        }
+//    }
 }
