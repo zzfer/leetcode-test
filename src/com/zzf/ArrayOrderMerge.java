9addbc21b1181fbc5f69074381db4f4220efb40c
zhaozhifei <1160306458@qq.com>
2022-05-04 18:24:57 +0800

    88. 合并两个有序数组

---
+++ src/com/zzf/ArrayOrderMerge.java
@@ -0,0 +1,89 @@
+package com.zzf;
+
+import java.util.Arrays;
+
+/**
+ * 88. 合并两个有序数组
+ * <p>
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * <p>
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * <p>
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ * 解释：需要合并 [1] 和 [] 。
+ * 合并结果是 [1] 。
+ * 示例 3：
+ * <p>
+ * 输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+ * 输出：[1]
+ * 解释：需要合并的数组是 [] 和 [1] 。
+ * 合并结果是 [1] 。
+ * 注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ *  
+ * <p>
+ * 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ArrayOrderMerge {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int[] nums3 = {0};
+        int[] nums4 = {1};
+
+        merge(nums3, 0, nums4, 1);
+        System.out.println(Arrays.toString(nums3));
+    }
+
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1, j = n - 1;
+        int length = m + n -1;
+        while (i >= 0 || j >= 0) {
+            if (i == -1) {
+                nums1[length--] = nums2[j--];
+            } else if (j == -1) {
+                nums1[length--] = nums1[i--];
+            } else if (nums1[i] > nums2[j]) {
+                nums1[length--] = nums1[i--];
+            } else {
+                nums1[length--] = nums2[j--];
+            }
+        }
+    }
+}
