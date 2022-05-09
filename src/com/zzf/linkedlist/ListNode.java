dbc0f8c1adf79d0c271e62e8a31d8765863fd9d9
zhaozhifei <1160306458@qq.com>
2022-05-09 18:15:02 +0800

    203. 移除链表元素

---
+++ src/com/zzf/linkedlist/ListNode.java
@@ -0,0 +1,45 @@
+package com.zzf.linkedlist;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListNode listNode = (ListNode) o;
+
+        if (val != listNode.val) return false;
+        return next != null ? next.equals(listNode.next) : listNode.next == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
+    }
+}
